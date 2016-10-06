2a538215b7020cc682f2d3c7f3fb6d2c84f407f1
zhangyutong926 <yotochang@gmail.com>
2016-10-06 20:30:05 +0800

    Wait a minute, I forgot something: commenting out old loadouts

--- src/daemon/conf_items.h
+++ src/daemon/conf_items.h
@@ -11,16 +11,16 @@
 
 //Configure variables defined in readconf.cpp
 
-extern char DATABASE_HOST[], DATABASE_USER[], DATABASE_PASS[], DATABASE_NAME[];
-extern char HTTP_BIND_IP[];
-extern uint16_t HTTP_BIND_PORT;
-
-extern bool lang_exist[];
-extern int lang_extra_mem[];
-extern std::string lang_ext[], lang_compiler[];
-
-extern char DataDir[];
-extern char TempDir[];
+//extern char DATABASE_HOST[], DATABASE_USER[], DATABASE_PASS[], DATABASE_NAME[];
+//extern char HTTP_BIND_IP[];
+//extern uint16_t HTTP_BIND_PORT;
+//
+//extern bool lang_exist[];
+//extern int lang_extra_mem[];
+//extern std::string lang_ext[], lang_compiler[];
+//
+//extern char DataDir[];
+//extern char TempDir[];
 
 // FIXME Perform a full-scale test to this new configuration system
 // FIXME Migrate system configuration from the extern variables above to the following new classes
@@ -49,7 +49,6 @@ public:
 //	static DaemonConfiguration GetInstance();
 
 private:
-
 	bool ParseIni(std::string);
 
 	bool ParseYaml(std::string);
